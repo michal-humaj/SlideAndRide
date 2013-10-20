64f6fb4e8c11fe16b7dfa7ec76590e4b12e00ee4
Michal Humaj <michal.humaj@gmail.com>
2013-10-20 19:19:23 +0200

    Initial commit

--- src/humaj/michal/MyActivity.java
+++ src/humaj/michal/MyActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
+     *
+     *
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
     }
 }
